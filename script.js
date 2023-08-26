91c20827a9739675e37f1afe75f7ba56e70f1588
Cody Lobdell <thecreator85@gmail.com>
2023-08-25 22:03:55 -0700

    added flower graphics and tuned x,y frames

--- script.js
+++ script.js
@@ -4,37 +4,77 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let drawing = false;
+
 class Root {
-    constructor(x, y){
-      this.x = x;
-      this.y = y;
-      this.speedX = Math.random() * 4 - 2;
-      this.speedX = Math.random() * 4 - 2;
-      this.maxSize = Math.random() * 7 + 5;
-      this.size = Math.random() * 1 + 2
-      this.vs = Math.random() * 0.2 + 0.05;
-      this.angleX = Math.random() * 6.2;
-      this.vax = Math.random() * 0.6 - 0.3;
-      this.angleY = Math.random() * 6.2;
-      this.vay = Math.random() * 0.6 - 0.3;
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.speedX = Math.random() * 4 - 2;
+    this.speedX = Math.random() * 4 - 2;
+    this.maxSize = Math.random() * 7 + 5;
+    this.size = Math.random() * 1 + 2
+    this.vs = Math.random() * 0.2 + 0.05;
+    this.angleX = Math.random() * 6.2;
+    this.vax = Math.random() * 0.6 - 0.3;
+    this.angleY = Math.random() * 6.2;
+    this.vay = Math.random() * 0.6 - 0.3;
+    this.lightness = 10;
+  }
+  update() {
+    this.x += this.speedX + Math.sin(this.angle);
+    this.y += this.speedY + Math.sin(this.angle);
+    this.size += this.vs;
+    this.angleX += this.vax;
+    this.angleY += this.vay;
+    if (this.lightness < 70) this.lightness += 0.25;
+    if (this.size < this.maxSize) {
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+      ctx.fillStyle = 'hsl(140,100%', + this.lightness + '%)';
+      ctx.fill();
+      ctx.stroke();
+      requestAnimationFrame(this.update.bind(this));
+    } else {
+      const flower = new Flower(this.x, this.y, this.size);
+      flower.grow();
     }
-    update(){
-      this.x += this.speedX + Math.sin(this.angle);
-      this.y += this.speedY + Math.sin(this.angle);
-      this.size += this.vs;
-      this.angleX += this.vax;
-      this.angleY += this.vay;
-      if (this.size < this.maxSize){
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-        ctx.fillStyle = 'hsl(140,100%,50%)';
-        ctx.fill();
-        ctx.stroke();
-        requestAnimationFrame(this.update.bind(this));  
-      }
-    }
+  }
 }
-window.addEventListener('mousemove', function(e){
-    const root = new Root(e.x, e.y);
-    root.update();
+
+class Flower {
+  constructor(x, y, size) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.maxFlowerSize = this.size + Math.random() * 50;
+    this.image = new Image();
+    this.image.src = 'flowers.png';
+    this.frameSize = 100;
+    this.frameX = Math.floor(Math.random() * 3);
+    this.frameY = Math.floor(Math.random() * 3);
+  };
+  grow() {
+    if (this.size < this.maxFlowerSize) {
+      this.size += 0.3;
+      ctx.drawImage(this.image, 0, 0, this.frameSize, this.frameSize, this.x, this.y, this.size, this.size);
+      requestAnimationFrame(this.grow.bind(this));
+    }
+  }
+}
+
+window.addEventListener('mousemove', function (e) {
+  if (drawing) {
+
+    for (let i = 0; i < 3; i++) {
+      const root = new Root(e.x, e.y);
+      root.update();
+    }
+  }
+});
+window.addEventListener('mousedown', function () {
+  drawing = true;
+});
+window.addEventListener('mouseup', function () {
+  drawing = false;
 });
