a37d4de39e60d2289358d6e211ff386556063cb9
Cody Lobdell <thecreator85@gmail.com>
2023-09-23 22:33:15 -0700

    added script for bloom and changed willflower

--- script.js
+++ script.js
@@ -47,17 +47,19 @@ class Flower {
     this.x = x;
     this.y = y;
     this.size = size;
-    this.maxFlowerSize = this.size + Math.random() * 50;
+    this.vs = Math.random() * 0.3 + 0.2;
+    this.maxFlowerSize = this.size + Math.random() * 80;
     this.image = new Image();
     this.image.src = 'flowers.png';
     this.frameSize = 100;
     this.frameX = Math.floor(Math.random() * 3);
     this.frameY = Math.floor(Math.random() * 3);
+    this.size > 11.5 ? this.willFlower = true : this.willFlower = false;
   };
   grow() {
-    if (this.size < this.maxFlowerSize) {
-      this.size += 0.3;
-      ctx.drawImage(this.image, 0, 0, this.frameSize, this.frameSize, this.x, this.y, this.size, this.size);
+    if (this.size < this.maxFlowerSize && this.willFlower) {
+      this.size += this.vs; 
+      ctx.drawImage(this.image, this.frameSize * this.frameX, this.frameSize * this.frameY, this.frameSize, this.frameSize, this.x, this.size/2, this.y, this.size/2, this.size, this.size);
       requestAnimationFrame(this.grow.bind(this));
     }
   }
